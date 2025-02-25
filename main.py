f3063c828e68ed7818b1d30eb8f24803e95bf7d3
Ben <perfectionofcreation@gmail.com>
2025-02-25 08:44:07 +0700

    123

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-#Include <iostream>
-using namespace std;
+import pygame as py
+import sys
 
-int main(){
-    pyout<<"Hello World";
-}
+py.init()
+
+screen = py.display.set_mode((800, 800), py.RESIZABLE)
+
+running = True
+while running:
+    for event in py.event.get():
+        if event.type == py.QUIT:
+            running = False
+
+    screen.fill(("red"))
+    screen.draw.polygon(screen, "magenta", )
+    py.display.flip()
+py.clock.tick(60)
+py.quit()     
