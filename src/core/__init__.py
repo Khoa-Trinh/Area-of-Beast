2fd8cab94a1fbc8d2195f784c223a82b220385a0
Khoa_Trinh_ThinkPad <kelvintrinh09@gmail.com>
2025-03-17 15:06:51 +0700

    Remove unused core and character modules to streamline the codebase

--- src/core/__init__.py
+++
@@ -1,2 +0,0 @@
-# FILE:/arena-of-beasts/arena-of-beasts/src/core/__init__.py
-# This file initializes the core module.
--- src/ui/__init__.py
+++
@@ -1,2 +0,0 @@
-# FILE:/arena-of-beasts/arena-of-beasts/src/ui/__init__.py
-# This file is intentionally left blank.
